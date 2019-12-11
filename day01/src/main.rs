82ee1a06702fe46b31f9e9628535f690da9f402b
Damon Barry <damonb@microsoft.com>
2019-12-11 12:38:11 -0800

    Run cargo fmt

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,5 +1,5 @@
-use std;
 use num;
+use std;
 
 fn calculate_fuel(mass: i64, include_fuel_mass: bool) -> i64 {
     let result = num::clamp((mass / 3) - 2, 0, std::i64::MAX);
@@ -12,7 +12,8 @@ fn calculate_fuel(mass: i64, include_fuel_mass: bool) -> i64 {
 }
 
 fn main() {
-    let result: i64 = std::fs::read_to_string("day01/src/input.txt").unwrap()
+    let result: i64 = std::fs::read_to_string("day01/src/input.txt")
+        .unwrap()
         .lines()
         .map(|l| calculate_fuel(l.parse().unwrap(), true))
         .sum();
