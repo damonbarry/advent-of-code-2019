bb9cdb64fa6b59a998df0d93b41129efc15bffe9
Damon Barry <damonb@microsoft.com>
2019-12-11 20:11:09 -0800

    Add a test for intcode computer halt

--- src/day02/mod.rs
+++ src/day02/mod.rs
@@ -1,5 +1,23 @@
+pub struct IntcodeComputer {
+    pub state: Vec<i64>
+}
+
+impl IntcodeComputer {
+    pub fn new() -> IntcodeComputer {
+        IntcodeComputer {
+            state: Vec::new()
+        }
+    }
+
+    pub fn run(&mut self, program: impl Iterator<Item = i64>) {
+        self.state = program.collect();
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn can_read_first_opcode() {
         let input = std::fs::read_to_string("src/day02/input.txt").unwrap();
@@ -8,5 +26,13 @@ mod tests {
             .map(|i| i.parse::<i64>().unwrap())
             .collect();
         assert_eq!(1, program[0]);
+    }
+
+    #[test]
+    fn computer_understands_halt() {
+        let program = [99];
+        let mut computer = IntcodeComputer::new();
+        computer.run(program.iter().cloned());
+        assert_eq!(&program[..], &computer.state[..]);
     }
 }
