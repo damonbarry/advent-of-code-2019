5e439194a23494e41b17ded44886e28e99d2bb5f
Damon Barry <damonb@microsoft.com>
2019-12-12 18:17:22 -0800

    Make sure all example programs work

--- src/day02/mod.rs
+++ src/day02/mod.rs
@@ -245,4 +245,50 @@ mod tests {
         assert!(result.is_ok());
         assert_eq!(&[2, 5, 6, 7, 99, 3, 7, 21], &program.state[..]);
     }
+
+    #[test]
+    fn runs_first_example_program() {
+        let instructions = [1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50];
+        let mut program = Program::new(instructions.to_vec());
+        let result = program.run();
+        assert!(result.is_ok());
+        assert_eq!(&[3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50], &program.state[..]);
+    }
+
+
+    #[test]
+    fn runs_second_example_program() {
+        let instructions = [1, 0, 0, 0, 99];
+        let mut program = Program::new(instructions.to_vec());
+        let result = program.run();
+        assert!(result.is_ok());
+        assert_eq!(&[2, 0, 0, 0, 99], &program.state[..]);
+    }
+
+    #[test]
+    fn runs_third_example_program() {
+        let instructions = [2, 3, 0, 3, 99];
+        let mut program = Program::new(instructions.to_vec());
+        let result = program.run();
+        assert!(result.is_ok());
+        assert_eq!(&[2, 3, 0, 6, 99], &program.state[..]);
+    }
+
+    #[test]
+    fn runs_fourth_example_program() {
+        let instructions = [2, 4, 4, 5, 99, 0];
+        let mut program = Program::new(instructions.to_vec());
+        let result = program.run();
+        assert!(result.is_ok());
+        assert_eq!(&[2, 4, 4, 5, 99, 9801], &program.state[..]);
+    }
+
+    #[test]
+    fn runs_fifth_example_program() {
+        let instructions = [1, 1, 1, 4, 99, 5, 6, 0, 99];
+        let mut program = Program::new(instructions.to_vec());
+        let result = program.run();
+        assert!(result.is_ok());
+        assert_eq!(&[30, 1, 1, 4, 2, 5, 6, 0, 99], &program.state[..]);
+    }
 }
