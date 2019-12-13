33070d16dc61eac241f622ea21223d4e24db206e
Damon Barry <damonb@microsoft.com>
2019-12-12 20:13:52 -0800

    Solve day2/part2 problem

--- src/day02/mod.rs
+++ src/day02/mod.rs
@@ -298,4 +298,40 @@ mod tests {
         assert!(result.is_ok());
         assert_eq!(8017076, program.state[0]);
     }
+
+    #[test]
+    fn solve_day2_part2() {
+        let input = std::fs::read_to_string("src/day02/input.txt").unwrap();
+        let init: Vec<i64> = input
+            .split(',')
+            .map(|i| i.parse::<i64>().unwrap())
+            .collect();
+
+        // for i in 0..99 {
+        //     for j in 0..99 {
+        //         let mut instructions = init.clone();
+        //         instructions[1] = i;
+        //         instructions[2] = j;
+
+        //         let mut program = Program::new(instructions);
+        //         let result = program.run();
+        //         assert!(result.is_ok());
+
+        //         if program.state[0] == 19690720 {
+        //             // to see this output: `cargo test -- --nocapture`
+        //             println!("### VALUES: [1] = {}, [2] = {}", i, j);
+        //             break;
+        //         }
+        //     }
+        // }
+
+        let mut instructions = init.clone();
+        instructions[1] = 31;
+        instructions[2] = 46;
+
+        let mut program = Program::new(instructions);
+        let result = program.run();
+        assert!(result.is_ok());
+        assert_eq!(19690720, program.state[0]);
+    }
 }
